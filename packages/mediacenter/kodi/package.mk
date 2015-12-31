bbcd1b9848cf953073a20ca960ba93c020d0f28c
Charliemarshall <charlie0440@gmail.com>
2015-12-31 16:00:38 +0000

    bump kodi

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="15.1-4416fe0"
+PKG_VERSION="15.2-6e52a28"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
